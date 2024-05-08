3495f0cbe9d79e246424e7a200d17a78ffbb1d9a
barkhudaryan-arman <armanik147@gmail.com>
2024-05-08 16:39:18 +0300

    исправил ДЗ!

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-    // Задача 1,2
+        // Задача 1,2,3
         {
             int[] first = new int[3];
             first[0] = 1;
@@ -15,6 +15,21 @@ public class Main {
                 }
                 System.out.print(first[i] + ", ");
             }
+            for (int i = first.length - 1; i >= 0; i--) {
+                System.out.print(first[i]);
+                if (i > 0) {
+                    System.out.print(", ");
+                }
+            }
+            //Задача 4
+            System.out.println();
+            for (int i = 0; i < first.length; i++) {
+                if (i % 2 == 0) {
+                    first[i] += 1;
+                }
+            }
+            System.out.println(Arrays.toString(first));
+            System.out.println();
         }
         {
             double[] second = {1.57, 7.654, 9.986};
@@ -25,66 +40,29 @@ public class Main {
                 }
                 System.out.print(second[i] + ", ");
             }
+            for (int i = second.length - 1; i >= 0; i--) {
+                System.out.print(second[i]);
+                if (i > 0) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
         }
         {
             int[] third = new int[]{2, 4, 16, 32, 64, 128, 256, 512};
-            for (int i = 0; i < third.length; i++){
+            for (int i = 0; i < third.length; i++) {
                 if (i == third.length - 1) {
                     System.out.println(third[i]);
                     break;
                 }
                 System.out.print(third[i] + ", ");
             }
-            }
-
-
-
-    // Задача 3
-        {
-            int[] first = new int[3];
-            first[0] = 1;
-            first[1] = 2;
-            first[2] = 3;
-            for (int i = first.length - 1; i >= 0; i--) {
-                System.out.print(first[i]);
-                if (i > 0){
-                    System.out.print(", ");
-                }
-            }
-            System.out.println();
-        }
-        {
-            double[] second = {1.57, 7.654, 9.986};
-                    for (int i = second.length - 1; i >= 0; i--) {
-                        System.out.print(second[i]);
-                        if (i > 0){
-                            System.out.print(", ");
-                        }
-                    }
-                    System.out.println();
-        }
-        {
-            int[] third = new int[]{2, 4, 16, 32, 64, 128, 256, 512};
             for (int i = third.length - 1; i >= 0; i--) {
                 System.out.print(third[i]);
-                if (i > 0){
+                if (i > 0) {
                     System.out.print(", ");
                 }
             }
-            System.out.println();
-        }
-
-
-
-        //Задача 4
-        {
-            int [] arr = {1, 2, 3, 6, 13};
-            for (int i = 0; i < arr.length; i++){
-                if(i % 2 == 0){
-                    arr[i] += 1;
-                }
-            }
-            System.out.println(Arrays.toString(arr));
         }
     }
 }
